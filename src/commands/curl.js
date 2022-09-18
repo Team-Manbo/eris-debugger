33b37348214d5fb3ce352bd90fe55ca4b3a56246
Manbo <77767945+Jam-Manbo@users.noreply.github.com>
2022-09-19 08:57:14 +0900

    (fix) typo - lowercase eris

--- src/commands/curl.js
+++ src/commands/curl.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const Eris = require('Eris')
+const Eris = require('eris')
 const { ProcessManager, HLJS } = require('../utils')
 
 module.exports = async function curl (message, parent) {
