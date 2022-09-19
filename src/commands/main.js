899b833d0f4ad6c0724777d34681b9073b6064c8
Manbo <77767945+Jam-Manbo@users.noreply.github.com>
2022-09-19 20:18:20 +0900

    (fix) typo (style) quotes

--- src/commands/main.js
+++ src/commands/main.js
@@ -7,13 +7,13 @@ module.exports = async function (message, parent) {
    let summary = `Eris-Debugger \`v${version}\`, Eris \`${Eris.VERSION}\`, Node.js \`${process.version}\` on \`${process.platform}\`.\nProcess started at ${DateFormatting.relative(system.processReadyAt())}, bot was ready at ${DateFormatting.relative(parent.client.startTime)}.\n`
 
   summary += `\nUsing \`${system.memory().rss}\` in this process.\n`
-  const cache = `${parent.client.guilds.size} guild(s) and ${parent.client.users.size} user(s)`
+  const cache = `${parent.client.guilds.size} guild${parent.client.guilds.size > 1 ? 's' : ''} and ${parent.client.users.size} user${parent.client.users.size > 1 ? 's' : ''}`
 
   /* Eris basically starts with internal sharding */
-  summary += `Running on PID ${process.pid} for this client, and running on PID ${process.ppid} for the parent process.\n\nThis bot is sharded in ${parent.client.shards.size} shard${parent.client.shards.size > 1 ? "s" : ""}.\n${cache} are cached this shard(\`#${message.channel.guild.shard.id}\` of total ${parent.client.shards.size}).`
+  summary += `Running on PID ${process.pid} for this client, and running on PID ${process.ppid} for the parent process.\n\nThis bot is sharded in ${parent.client.shards.size} shard${parent.client.shards.size > 1 ? 's' : ''}.\n${cache} are cached in this shard(\`#${message.channel.guild.shard.id}\` of total ${parent.client.shards.size}).`
 
 
-  summary += `\n\nMax Listerners Count: \`${parent.client.getMaxListeners()}\`\nAverage shard latency: \`${message.channel.guild.shard.latency}\`ms\nAverage websocket latency: \`${message.channel.guild.shard.latency}\`ms`
+  summary += `\n\nMax listerners count: \`${parent.client.getMaxListeners()}\`\nAverage websocket latency: \`${message.channel.guild.shard.latency}\`ms\nAverage REST request latency: \`${parent.client.latencyRef.latency}\`ms`
 
   return message.channel.createMessage(summary)
 }
